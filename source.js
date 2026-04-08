544993e4e062564840da81ad271e2272db8e9816
Aviralvats <aviralvats85@gmail.com>
2026-04-08 14:33:36 +0530

    Add files via upload

---
+++ source.js
@@ -0,0 +1,13 @@
+
+let timer=Document.EventListener.getElementById(("start"),()=>{
+         let a=input()
+         for(let i=a;i<0;i--){
+            Document.write(i)
+            console.log(i)
+ }
+ 
+
+
+
+
+})
